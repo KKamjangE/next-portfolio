c4bdfd9827a66d766b75de48ff0363e79f283cde
KKamjangE <ajm980718@gmail.com>
2024-06-21 17:45:03 +0900

    Feat: date format 유틸 함수 생성

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatToTimeAge(date: string): string {
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const time = new Date(date).getTime();
+    const now = new Date().getTime();
+    const diff = Math.round((time - now) / dayInMs);
+
+    const formatter = new Intl.RelativeTimeFormat("ko");
+
+    return formatter.format(diff, "days");
+}
