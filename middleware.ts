ee334119ffafb82b5e2fffe6fdda15d5c91124dd
KKamjangE <ajm980718@gmail.com>
2024-06-21 16:38:43 +0900

    Feat: middleware 생성, sign page 보호

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+    function middleware() {
+        return NextResponse.next();
+    },
+    {
+        callbacks: {
+            authorized: ({ req }) => {
+                return !Boolean(req.cookies.get("next-auth.session-token"));
+            },
+        },
+        secret: process.env.NEXTAUTH_SECRET,
+        pages: {
+            signIn: "/",
+        },
+    },
+);
+
+export const config = {
+    matcher: ["/sign"],
+};
