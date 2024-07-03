4d27bc40e575a723455884d0ca24d19f93005ad5
KKamjangE <ajm980718@gmail.com>
2024-07-03 13:45:23 +0900

    Feat: system theme 분기처리

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -3,12 +3,24 @@
 import { Button } from "@/components/ui/button"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { useEffect } from "react"
 
 export default function ThemeButton() {
-    const { theme, setTheme } = useTheme()
+    const { theme, systemTheme, setTheme } = useTheme()
+
+    useEffect(() => {
+        console.log(theme)
+        console.log("system", systemTheme)
+    }, [theme, systemTheme])
 
     const onChangeTheme = () => {
-        setTheme(theme === "dark" ? "light" : "dark")
+        let currentTheme
+        if (theme === "system") {
+            currentTheme = systemTheme
+        } else {
+            currentTheme = theme
+        }
+        setTheme(currentTheme === "dark" ? "light" : "dark")
     }
 
     return (
@@ -16,7 +28,7 @@ export default function ThemeButton() {
             variant={"ghost"}
             size={"icon"}
             onClick={onChangeTheme}
-            className="transition duration-300 ease-in-out"
+            className="transition duration-150"
         >
             <Sun className="rotate-0 scale-100 transition dark:-rotate-180 dark:scale-0" />
             <Moon className="absolute rotate-180 scale-0 transition dark:rotate-0 dark:scale-100" />
