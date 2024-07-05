613c40ddd1e9c9434e54aa1db06f60e364d10291
KKamjangE <ajm980718@gmail.com>
2024-07-05 18:02:45 +0900

    Feat: project article 컴포넌트 수정

--- components/project-article.tsx
+++ components/project-article.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { formatToKRDate } from "@/lib/utils"
 import Link from "next/link"
 
@@ -22,30 +23,38 @@ export default function ProjectArticle({
         <article className="rounded-md border border-neutral-700 bg-blue-950/30 p-5">
             <h2 className="text-2xl font-semibold">{title}</h2>
             <p>{description}</p>
+            <div className="flex justify-center gap-3">
+                {github && (
+                    <Button variant={"link"}>
+                        <Link href={github} target="_blank">
+                            Github
+                        </Link>
+                    </Button>
+                )}
+                {blog && (
+                    <Button variant={"link"}>
+                        <Link href={blog} target="_blank">
+                            Blog
+                        </Link>
+                    </Button>
+                )}
+                {site && (
+                    <Button variant={"link"}>
+                        <Link href={site} target="_blank">
+                            Site
+                        </Link>
+                    </Button>
+                )}
+            </div>
+            <div className="flex justify-center">
+                {skills.map((skill) => (
+                    <p key={skill.name}>{skill.name}</p>
+                ))}
+            </div>
             <p className="text-sm">
                 {formatToKRDate(startDate)}~
                 {formatToKRDate(endDate) ?? "진행 중"}
             </p>
-            <div className="flex gap-3">
-                {github && (
-                    <Link href={github} target="_blank">
-                        Github
-                    </Link>
-                )}
-                {blog && (
-                    <Link href={blog} target="_blank">
-                        Blog
-                    </Link>
-                )}
-                {site && (
-                    <Link href={site} target="_blank">
-                        Site
-                    </Link>
-                )}
-            </div>
-            {skills.map((skill) => (
-                <p key={skill.name}>{skill.name}</p>
-            ))}
         </article>
     )
 }
