0adc9ea4b884fdaf0c292425c83f5e3edfba2d3c
KKamjangE <ajm980718@gmail.com>
2024-10-18 12:55:32 +0900

    Feat: spline 테스트

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,14 @@
-import { getProjects } from "@/app/(home)/actions"
-import ProjectArticle from "@/app/(home)/project-article"
+import Spline from "@splinetool/react-spline/next"
 
 export default async function Home() {
-    const projects = await getProjects()
     return (
         <div>
-            <h1>본인 소개</h1>
+            <Spline scene="https://prod.spline.design/Z-rpjFo7KruOYKch/scene.splinecode" />
+            <h1>안녕하세요. 기록과 소통을 좋아하는 안제민입니다.</h1>
+            <p>배운 것을 기록하고 공유하는 것을 좋아합니다.</p>
+            <p>항상 팀원들과 원활한 소통을 위해 노력합니다.</p>
             <section>
                 <h1>참여한 서비스, 프로젝트 소개</h1>
-                {projects.map((project) => (
-                    <ProjectArticle
-                        key={project.id}
-                        descriptions={project.descriptions}
-                        title={project.title}
-                        startDate={project.startDate}
-                        endDate={project.endDate}
-                        urls={project.urls!}
-                        skills={project.skills}
-                    />
-                ))}
             </section>
             <section>
                 <h1>일과 관련된 경험 소개</h1>
