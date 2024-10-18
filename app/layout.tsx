95e5948eb4a633e40281603c087cd23f7f75d125
KKamjangE <ajm980718@gmail.com>
2024-10-18 12:56:10 +0900

    Fix: root layout side-navigation 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import { Noto_Sans_KR } from "next/font/google"
 
 import Header from "@/components/header"
-import SideNavigationMenu from "@/components/navigation/side-navigation-menu"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import "./globals.css"
@@ -10,8 +9,8 @@ import "./globals.css"
 const notoSansKr = Noto_Sans_KR({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: "안제민 포트폴리오",
-    description: "프론트엔드 엔지니어 안제민입니다.",
+    title: "Jemin's Docs",
+    description: "안녕하세요! 안제민입니다.",
 }
 
 export default function RootLayout({
@@ -29,7 +28,6 @@ export default function RootLayout({
                 >
                     <Header />
                     <div className="container mx-auto flex-1 gap-8 md:grid md:grid-cols-[220px_minmax(0,1fr)]">
-                        <SideNavigationMenu />
                         <main className="mt-10 p-3">{children}</main>
                     </div>
                 </ThemeProvider>
