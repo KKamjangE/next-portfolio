87d25ce931eea27b181677a792c236a9c0d0cefb
KKamjangE <ajm980718@gmail.com>
2024-10-18 12:57:08 +0900

    Feat: site config 파일 생성

---
+++ app/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+    name: "Jemin's Docs",
+    description: "",
+    links: {
+        velog: "",
+        github: "",
+    },
+}
+
+export type SiteConfig = typeof siteConfig
