7dbe5e4d06cba5e8df947799cbed4eff154e2db5
KKamjangE <ajm980718@gmail.com>
2024-11-05 11:11:42 +0900

    Feat: header 컴포넌트 contents 그룹으로 이동

--- app/(contents)/layout.tsx
+++ app/(contents)/layout.tsx
@@ -1,9 +1,14 @@
+import Header from "@/components/header"
+
 export default function ContentsLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className="container mt-3 flex-1 gap-8 p-3 md:grid md:grid-cols-[220px_minmax(0,1fr)]">
-            {children}
-        </div>
+        <>
+            <Header />
+            <div className="container mt-3 flex-1 gap-8 p-3 md:grid md:grid-cols-[220px_minmax(0,1fr)]">
+                {children}
+            </div>
+        </>
     )
 }
