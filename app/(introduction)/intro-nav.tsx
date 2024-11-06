c5e7b6bacfdfd20f7e2bf22a6bc352d5d2e97be0
KKamjangE <ajm980718@gmail.com>
2024-11-06 17:04:04 +0900

    Style: intro-nav 반응형 스타일링

--- app/(introduction)/intro-nav.tsx
+++ app/(introduction)/intro-nav.tsx
@@ -5,11 +5,11 @@ import { siteConfig } from "@/app/config/site"
 export default function IntroNav() {
     return (
         <>
-            <div className="absolute left-10 right-10 top-10 flex justify-between align-middle">
-                <h2 className="text-5xl font-semibold">
+            <div className="absolute left-4 right-4 top-4 flex justify-between align-middle md:left-10 md:right-10 md:top-10">
+                <h2 className="text-3xl font-semibold md:text-5xl">
                     Jemin&apos;s PortFolio
                 </h2>
-                <nav className="flex gap-12 *:text-4xl *:font-semibold">
+                <nav className="flex gap-6 *:text-2xl *:font-semibold md:gap-12 *:md:text-4xl">
                     <a href={siteConfig.links.github} target="_blank">
                         <span>Github</span>
                     </a>
@@ -18,7 +18,7 @@ export default function IntroNav() {
                     </a>
                 </nav>
             </div>
-            <div className="absolute bottom-10 left-10 text-2xl font-semibold">
+            <div className="absolute bottom-4 left-4 text-xl font-semibold md:bottom-10 md:left-10 md:text-2xl">
                 lastupdated: https://github.com/orgs/vercel/discussions/587
             </div>
         </>
