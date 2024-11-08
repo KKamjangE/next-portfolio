67a995c011e708e8d91dc7be0a6a0a343bcd8556
KKamjangE <ajm980718@gmail.com>
2024-11-08 17:50:09 +0900

    Chore: svg webpack 설정

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,29 @@ const nextConfig = {
             },
         ],
     },
+    webpack: (config) => {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.(".svg"),
+        )
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: {
+                    not: [...fileLoaderRule.resourceQuery.not, /url/],
+                },
+                use: ["@svgr/webpack"],
+            },
+        )
+
+        return config
+    },
 }
 
 export default nextConfig
