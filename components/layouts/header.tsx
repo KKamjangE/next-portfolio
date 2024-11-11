f309922ae30d46097fd19eada283cb9b7671b032
KKamjangE <ajm980718@gmail.com>
2024-11-11 15:03:02 +0900

    Style: header z-index 수정

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     const session = await getAuthSession()
 
     return (
-        <header className="sticky top-0 z-50 border-b border-neutral-600 backdrop-blur">
+        <header className="sticky top-0 z-[500] border-b border-neutral-600 backdrop-blur">
             <div className="m-auto flex h-12 max-w-screen-lg items-center justify-between px-5 *:flex *:items-center">
                 <div>
                     <MainSideNav />
