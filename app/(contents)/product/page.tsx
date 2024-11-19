832c4028d5a274733c9d24636f424f3e4d8cda50
KKamjangE <ajm980718@gmail.com>
2024-11-19 17:19:16 +0900

    Remove: site link, project-article 컴포넌트 제거

--- app/(contents)/product/page.tsx
+++ app/(contents)/product/page.tsx
@@ -4,9 +4,5 @@ import NotionViewer from "@/app/(contents)/product/notion-viewer"
 export default async function Product() {
     const page = await getNotionPage("13805fa11ff880b8addddc32b66c9a8c")
 
-    return (
-        <div>
-            <NotionViewer data={page} />
-        </div>
-    )
+    return <NotionViewer data={page} />
 }
