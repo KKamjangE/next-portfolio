d9140f39e8d1af612eebb7f5d8f33c534fbc021c
KKamjangE <ajm980718@gmail.com>
2024-11-19 18:07:55 +0900

    nav-item 컴포넌트 li 태그 제거

--- components/layouts/nav-item.tsx
+++ components/layouts/nav-item.tsx
@@ -18,14 +18,12 @@ export default function NavItem({
     const pathname = usePathname()
 
     return (
-        <li>
-            <Link
-                href={href}
-                onClick={onClick}
-                className={`text-md ${pathname == href ? "opacity-100" : "opacity-40"} transition-opacity hover:opacity-100`}
-            >
-                {children}
-            </Link>
-        </li>
+        <Link
+            href={href}
+            onClick={onClick}
+            className={`text-md ${pathname == href ? "opacity-100" : "opacity-40"} transition-opacity hover:opacity-100`}
+        >
+            {children}
+        </Link>
     )
 }
