230b716ab8cc5a0151edc7131fd07d8429220307
KKamjangE <ajm980718@gmail.com>
2024-11-28 10:04:02 +0900

    우측 콘텐츠 반응형 디자인

--- app/(contents)/product/page.tsx
+++ app/(contents)/product/page.tsx
@@ -1,6 +1,5 @@
 import { getNotionPage } from "@/app/(contents)/product/actions"
 import NotionViewer from "@/app/(contents)/product/notion-viewer"
-import ProductNav from "@/app/(contents)/product/product-nav"
 
 export default async function Product() {
     const page = await getNotionPage("13805fa11ff880b8addddc32b66c9a8c")
@@ -8,7 +7,7 @@ export default async function Product() {
     return (
         <div className="flex gap-8">
             <NotionViewer page={page} />
-            <div className="w-[220px]">오른쪽 콘텐츠</div>
+            <div className="hidden w-[220px] xl:block"></div>
         </div>
     )
 }
